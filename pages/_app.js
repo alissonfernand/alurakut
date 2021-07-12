3bb422c59573cc457692402b26f7587f4e6df6cc
alissonfernandes <alisson.flutter@gmail.com>
2021-07-12 15:19:18 -0300

    finalizando aula1

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,37 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import {AlurakutStyles} from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
+/* Reset CSS */
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
   body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+   font-family: sans-serif;
+   background-color: #D9E6F6;
   }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  img {
+    display: flex;
+    max-width: 100%;
+    height: auto;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: 'red',
   },
 }
 
